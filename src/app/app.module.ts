0db46aa3c79a339810d15f0da43f34cca311b359
Koshy <624865@cognizant.com>
2018-12-04 15:32:21 +0530

    To-do-list app updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { GrandParentComponent } from './family/grand-parent/grand-parent.component';
 import { FatherComponent } from './family/father/father.component';
 import { MotherComponent } from './family/mother/mother.component';
 import { TaskManagerComponent } from './mini-app/task-manager/task-manager.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,46 @@ import { TaskManagerComponent } from './mini-app/task-manager/task-manager.compo
     GrandParentComponent,
     FatherComponent,
     MotherComponent,
-    TaskManagerComponent
+    TaskManagerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        redirectTo: 'family',
+        pathMatch: 'full'
+      },
+      {
+        path:'family',
+        component: GrandParentComponent,
+        children:[
+          {
+            path:'',
+            redirectTo: 'father',
+            pathMatch: 'full'
+          },
+          {
+            path:'father',
+            component: FatherComponent
+          },
+          {
+            path:'mother',
+            component: MotherComponent
+          }
+        ]
+      },
+       {
+        path:'task',
+        component: TaskManagerComponent
+      },
+       {
+        path:'**',
+        component: NotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
