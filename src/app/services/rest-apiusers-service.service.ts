e0d686d7308559c644800b78bcdc51fa343003cc
Koshy <624865@cognizant.com>
2018-12-06 17:06:09 +0530

    plp update

--- src/app/services/rest-apiusers-service.service.ts
+++ src/app/services/rest-apiusers-service.service.ts
@@ -9,17 +9,17 @@ export class RestAPIUsersServiceService {
   constructor(private http:HttpClient) { }
 
   getRemoteUsers(){
-    return this.http.get("http://localhost:3000/users")
+    return this.http.get("http://localhost:3001/users")
   }
 
   createUser(user){
-    return this.http.post("http://localhost:3000/users", user)
+    return this.http.post("http://localhost:3001/users", user)
   }
   removeUser(indexRec){
-    return this.http.delete("http://localhost:3000/users/" + indexRec)
+    return this.http.delete("http://localhost:3001/users/" + indexRec)
   }
   getOneUser(id){
-    return this.http.get("http://localhost:3000/users/" + id)
+    return this.http.get("http://localhost:3001/users/" + id)
   }
   getRemoteProducts(){
     return this.http.get("http://localhost:3000/products")
